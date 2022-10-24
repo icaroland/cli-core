f5d477f2bc041e8f1d833846f248cf0c214dcca8
LGala <galafassilorenzo@gmail.com>
2022-10-24 21:08:35 +0200

    start feat

--- src/main/kotlin/commands/LangUse.kt
+++ src/main/kotlin/commands/LangUse.kt
@@ -2,7 +2,7 @@ package commands
 
 import picocli.CommandLine
 
-@CommandLine.Command(name = "lang-use", description = ["From now the code will be compiled with the given language version"])
+@CommandLine.Command(name = "lang-use", description = ["From now on the code will be compiled with the given language version"])
 class LangUse : Runnable {
     @CommandLine.Parameters(paramLabel = "<version>", description = ["language version"])
     lateinit var langVersion: String
