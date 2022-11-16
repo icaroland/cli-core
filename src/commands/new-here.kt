8de13d9eb72a3ca8e1a4af021153f9c03eb36063
LGala <galafassilorenzo@gmail.com>
2022-11-16 23:46:52 +0100

    keep going

--- src/commands/new-here.kt
+++ src/commands/new-here.kt
@@ -3,45 +3,53 @@ package commands
 import BINARY_DIR_NAME
 import CLASSES_DIR_NAME
 import CLI_VERSION_DEPS_ATTRIBUTE_NAME
-import DEPS_FILE_NAME
+import DEPS_FILE
 import LANG_VERSION_DEPS_ATTRIBUTE_NAME
-import MAIN_SOURCE_FILE_NAME
+import MAIN_SOURCE_FILE
 import SOURCE_DIR_NAME
 import com.google.gson.GsonBuilder
 import picocli.CommandLine
 import java.io.File
-import java.nio.file.Files
-import java.nio.file.Path
 
 @CommandLine.Command(name = "new-here", description = ["the current directory will become an Icaro project"])
 class NewHere : Runnable {
     override fun run() {
         try {
-            createDependenciesFile()
+            print("insert cli version: ")
+            val cliVersion = readLine()!!
 
-            File(SOURCE_DIR_NAME).mkdir()
-            File("$SOURCE_DIR_NAME/$MAIN_SOURCE_FILE_NAME.ic").writeText("")
-            
-            File(".$BINARY_DIR_NAME/$CLASSES_DIR_NAME").mkdirs()
+            print("insert lang version: ")
+            val langVersion = readLine()!!
+
+            createDependenciesFile(cliVersion, langVersion)
+            createSrcAndBinDirs(SOURCE_DIR_NAME, BINARY_DIR_NAME)
         } catch (e: Throwable) {
             e.printStackTrace()
         }
     }
+}
 
-    private fun createDependenciesFile() {
-        print("insert cli version: ")
-        val cliVersion = readLine()!!
+/*
+doesn't make sense to pass constants but
+1. constants are useful
+2. in this case, we make this function parametrized, or we couldn't test it 
+   because creating a src directory in the root path of this folder conflict with this 
+   project src file (DX with docker is bad because everytime we need to wait for maven to build)
+ */
+fun createSrcAndBinDirs(sourceDirName: String, binaryDirName: String) {
+    File(sourceDirName).mkdir()
+    File("$sourceDirName/$MAIN_SOURCE_FILE").writeText("")
 
-        print("insert lang version: ")
-        val langVersion = readLine()!!
+    File("$binaryDirName/$CLASSES_DIR_NAME").mkdirs()
+}
 
-        val depsInJson = GsonBuilder().setPrettyPrinting().create().toJson(
-            mapOf(
-                CLI_VERSION_DEPS_ATTRIBUTE_NAME to cliVersion,
-                LANG_VERSION_DEPS_ATTRIBUTE_NAME to langVersion
-            )
+fun createDependenciesFile(cliVersion: String, langVersion: String) {
+    val depsInJson = GsonBuilder().setPrettyPrinting().create().toJson(
+        mapOf(
+            CLI_VERSION_DEPS_ATTRIBUTE_NAME to cliVersion,
+            LANG_VERSION_DEPS_ATTRIBUTE_NAME to langVersion
         )
+    )
 
-        File("$DEPS_FILE_NAME.json").writeText(depsInJson)
-    }
+    File(DEPS_FILE).writeText(depsInJson)
 }
--- test/commands/new-here.kt
+++ test/commands/new-here.kt
@@ -1,5 +1,43 @@
 package commands
 
+import CLASSES_DIR_NAME
+import CLI_VERSION_DEPS_ATTRIBUTE_NAME
+import DEPS_FILE
+import LANG_VERSION_DEPS_ATTRIBUTE_NAME
+import MAIN_SOURCE_FILE
+import com.google.gson.Gson
+import org.junit.jupiter.api.Test
+import java.io.File
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
 class NewHereTest {
-    
+    @Test
+    fun shouldCreateDependenciesFile() {
+        val cliVersion = "1.2"
+        val langVersion = "3.1-rc.3"
+
+        createDependenciesFile(cliVersion, langVersion)
+
+        val dependencies = Gson().fromJson(File(DEPS_FILE).readText(), Map::class.java)
+
+        assertEquals(cliVersion, dependencies[CLI_VERSION_DEPS_ATTRIBUTE_NAME])
+        assertEquals(langVersion, dependencies[LANG_VERSION_DEPS_ATTRIBUTE_NAME])
+
+        File(DEPS_FILE).delete()
+    }
+
+    @Test
+    fun shouldCreateSourceAndBinaryDirectories() {
+        val sourceDir = "src43234"
+        val binDir = ".bin43334"
+        
+        createSrcAndBinDirs(sourceDir, binDir)
+
+        assertTrue(File("$sourceDir/$MAIN_SOURCE_FILE").isFile)
+        assertTrue(File("$binDir/$CLASSES_DIR_NAME").isDirectory)
+
+        File(sourceDir).deleteRecursively()
+        File(binDir).deleteRecursively()
+    }
 }
